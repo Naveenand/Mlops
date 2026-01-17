8be30cf3beacbe58f23c8de1ba08e57ac84817fa
Naveen <naveenyadavgolla04@gmail.com>
2026-01-17 00:30:55 -0600

    requirement added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+
+    name = 'heart_failure',
+    version = "0.0.0",
+    author=  "Naveen",
+    author_email= 'naveenyadavgolla04@gmail.com',
+    packages=find_packages()
+)
